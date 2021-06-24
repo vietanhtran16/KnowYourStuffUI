c66f4e08db400b1ac03973864d73dbd480a084c5
Viet Anh Tran <tranvietanh1996@gmail.com>
2021-06-25 08:53:35 +1200

    Add bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const prod = process.env.NODE_ENV === "production";
 
@@ -30,6 +31,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html'
         }),
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin(),
+        new BundleAnalyzerPlugin()
     ]
 }
