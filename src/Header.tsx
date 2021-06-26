583b7b9e3d47b838e41af4276fdbc66380938fa0
Viet Anh Tran <tranvietanh1996@gmail.com>
2021-06-26 19:27:35 +1200

    Change appbar color

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,26 @@
-import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
 import React from "react";
 
-export const Header = () => (
-  <AppBar position="sticky">
-    <Toolbar>
-      <IconButton edge="start" color="inherit" aria-label="menu"></IconButton>
-      <Typography variant="h6">Know Your Stuff Wiki</Typography>
-    </Toolbar>
-  </AppBar>
-);
+const useStyles = makeStyles({
+  appBar: {
+    background: "#484848",
+  },
+});
+
+export const Header = () => {
+  const classes = useStyles();
+  return (
+    <AppBar className={classes.appBar} position="sticky">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu"></IconButton>
+        <Typography variant="h6">Know Your Stuff Wiki</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
