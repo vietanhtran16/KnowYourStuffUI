09ff2b34a4ca272a239647faae98f4b42730dc3d
Viet Anh Tran <tranvietanh1996@gmail.com>
2021-10-23 22:39:25 +1300

    Optimisticly update tips cache and invalidate cache once post completed

--- src/hooks/tips.ts
+++ src/hooks/tips.ts
@@ -1,9 +1,30 @@
-import { useMutation, useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import { createTip, getPlatformTips, NewTip, Tip } from "./../services/api";
 
-export const useTips = (platformId: string) => {
-    return useQuery(["platform-tips", platformId], () => getPlatformTips(platformId));
+const getTipQueryKey = (platformId: string): string[] => {
+    return ["platform-tips", platformId];
 };
 
-export const useCreateTip = () =>
-    useMutation<Tip, unknown, { tip: NewTip; platformId: string }>(({ tip, platformId }) => createTip(tip, platformId));
+export const useTips = (platformId: string) => {
+    return useQuery(getTipQueryKey(platformId), () => getPlatformTips(platformId));
+};
+
+export const useCreateTip = () => {
+    const queryClient = useQueryClient();
+    return useMutation<Tip, unknown, { tip: NewTip; platformId: string }>(
+        ({ tip, platformId }) => createTip(tip, platformId),
+        {
+            onMutate: async ({ tip, platformId }) => {
+                const cacheKey = getTipQueryKey(platformId);
+                await queryClient.cancelQueries(cacheKey);
+                const optimisticNewTip: Tip = { id: "sdfdfdfdf", ...tip, platformId };
+                queryClient.setQueryData<Tip[]>(cacheKey, (current) =>
+                    current ? [...current, optimisticNewTip] : [optimisticNewTip],
+                );
+            },
+            onSuccess: ({ platformId }) => {
+                queryClient.invalidateQueries(getTipQueryKey(platformId));
+            },
+        },
+    );
+};
